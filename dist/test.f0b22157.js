de6786b95565e505633d88966c4bccc4d3fb3bf5
gggyy3 <1587644910@qq.com>
2022-02-18 11:00:29 +0800

    update

--- dist/test.f0b22157.js
+++ dist/test.f0b22157.js
@@ -212,7 +212,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = '' || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + '4571' + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + '5630' + '/');
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
 
